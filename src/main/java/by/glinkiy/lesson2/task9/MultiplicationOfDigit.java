73497e84ce9fb11d7d61ede5c0403e2d4ad6e925
Ilya Glinskiy <Glnskiilya@yandex.ru>
2021-02-28 17:20:50 +0300

    task9

---
+++ src/main/java/by/glinkiy/lesson2/task9/MultiplicationOfDigit.java
@@ -0,0 +1,19 @@
+package by.glinkiy.lesson2.task9;
+
+/**
+ * Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а
+ * произведение, т.е. факториал числа.
+ */
+public class MultiplicationOfDigit {
+    public static void main(String[] args) {
+        int n = 0;
+        int sumMult = 1;
+        int x = (int) (Math.random() * 20);
+        while (x != 0) {
+            sumMult *= x;
+            n++;
+            x = (int) (Math.random() * 20);
+        }
+        System.out.println(sumMult);
+    }
+}
