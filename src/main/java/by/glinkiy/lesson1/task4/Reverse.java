5177d1ba0b5dd5459a255e6d0b5a07097796ff5f
Ilya Glinskiy <Glnskiilya@yandex.ru>
2021-02-28 17:12:04 +0300

    task4

--- src/main/java/by/glinkiy/lesson1/task4/Reverse.java
+++ src/main/java/by/glinkiy/lesson1/task4/Reverse.java
@@ -1,4 +1,20 @@
 package by.glinkiy.lesson1.task4;
 
+/**
+ * Определить число, полученное выписыванием в обратном порядке цифр
+ любого 4-х значного натурального числа n.
+ */
 public class Reverse {
+    public static void main(String[] args) {
+        int a= 1234;
+        for (int i = 0; i < 4; i++) {
+            System.out.print(a%10);
+            int t=a/10;
+            a=t;
+
+
+        }
+
+
+    }
 }
