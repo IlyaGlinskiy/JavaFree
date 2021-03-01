db901481f0c1b5ac6e29e07464f5d38cd32b411f
Ilya Glinskiy <Glnskiilya@yandex.ru>
2021-03-01 20:21:54 +0300

    Task 1 add Variables

--- src/main/java/by/glinkiy/lesson1/task1/HelloWorld.java
+++ src/main/java/by/glinkiy/lesson1/task1/HelloWorld.java
@@ -2,7 +2,8 @@ package by.glinkiy.lesson1.task1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String s = "Hello World";
+        System.out.println(s);
 
     }
 }
