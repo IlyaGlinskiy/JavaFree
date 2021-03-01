edc30c94e96373bafbca46d9fc4acc448eb9bf28
Ilya Glinskiy <Glnskiilya@yandex.ru>
2021-03-01 07:27:23 +0300

    task12

---
+++ src/main/java/by/glinkiy/lesson2/task12/DivisionOnThree.java
@@ -0,0 +1,19 @@
+package by.glinkiy.lesson2.task12;
+
+/**
+ * Найдите сумму первых n целых чисел, которые делятся на 3.
+ */
+public class DivisionOnThree {
+    public static void main(String[] args) {
+        int x = 10;
+        int sum = 0;
+        for (int i = 0; i < x; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            } else {
+                continue;
+            }
+        }
+        System.out.println(sum);
+    }
+}
